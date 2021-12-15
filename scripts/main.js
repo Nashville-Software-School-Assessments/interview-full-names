ba3999a43bee8d1836688f62b8a21d663d30d9c9
Steve Brownlee <chortlehoort@gmail.com>
2021-12-15 17:26:30 -0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+/*
+    Write a function that returns the concatenation of the first and last
+    name of each customer object that is provided to the function. Only active
+    customers should be returned. If the customer is not active, return the
+    string of "inactive".
+
+    Below are two arrays of customers. Your function should be able to work
+    for any array. Verify that it works for either array in your solution by
+    completing the code at the bottom of the module.
+*/
+
+
+// Write your function here to return full names
+
+
+
+const bankCustomers = [
+    { id: 1, name: { first: "Ambros", last: "Zanre" }, email: "azanre0@forbes.com", active: false },
+    { id: 2, name: { first: "Dermot", last: "Sweetnam" }, email: "dsweetnam1@fda.gov", active: true },
+    { id: 3, name: { first: "Shirlee", last: "Lis" }, email: "slis2@baidu.com", active: true },
+    { id: 4, name: { first: "Lurline", last: "Tucsell" }, email: "ltucsell3@comcast.net", active: false },
+    { id: 5, name: { first: "Tarra", last: "Antonucci" }, email: "tantonucci4@cbsnews.com", active: false },
+    { id: 6, name: { first: "Lucina", last: "Oglesbee" }, email: "loglesbee5@rambler.ru", active: false },
+    { id: 7, name: { first: "Blair", last: "Palser" }, email: "bpalser6@accuweather.com", active: true },
+    { id: 8, name: { first: "Erna", last: "Nuzzti" }, email: "enuzzti7@bigcartel.com", active: false },
+    { id: 9, name: { first: "Joshia", last: "Chatwood" }, email: "jchatwood8@xing.com", active: false },
+    { id: 10, name: { first: "Asher", last: "Martinello" }, email: "amartinello9@buzzfeed.com", active: false }
+]
+
+
+const ticketHolders = [
+    { id: 1, current: true, name: { first: "Lindi", last: "Blaksland" }, age: 37 },
+    { id: 2, current: true, name: { first: "Pebrook", last: "Pidgley" }, age: 44 },
+    { id: 3, current: true, name: { first: "Shae", last: "Lauret" }, age: 55 },
+    { id: 4, current: false, name: { first: "Elizabeth", last: "Beedon" }, age: 32 },
+    { id: 5, current: false, name: { first: "Freedman", last: "Humberstone" }, age: 44 },
+    { id: 6, current: true, name: { first: "Gunter", last: "Ciementini" }, age: 29 },
+    { id: 7, current: false, name: { first: "Agnes", last: "Verbeek" }, age: 49 },
+    { id: 8, current: false, name: { first: "Conrad", last: "Carcass" }, age: 55 },
+    { id: 9, current: true, name: { first: "Jody", last: "Brocket" }, age: 44 },
+    { id: 10, current: false, name: { first: "Phillip", last: "Shackell" }, age: 53 }
+]
+
+// Use `console.log()` to display the full name of each ticket holder
+for ( of ) {
+}
+
+
+// Use `console.log()` to display the full name of each bank customer
+for ( of ) {
+
+}
